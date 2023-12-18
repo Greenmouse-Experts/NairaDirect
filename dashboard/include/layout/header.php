d63f7040a3f0ab50976eb6c72278a26a3f90022c
Hamzat Abdulazeez <95409350+HamzatAbdulazeez@users.noreply.github.com>
2023-12-18 11:49:17 +0100

    Update

---
+++ dashboard/include/layout/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./js/bootstrap.js">
+    <link rel="stylesheet" href="./css/dashstyle.css">
+    <title>Header</title>
+</head>
+</head>
+<body>
+    <header>
+        Welcome Header
+    </header>
+</body>
+</html>
--- dashboard/welcome.php
+++ dashboard/welcome.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/bootstrap.css">
     <link rel="stylesheet" href="./js/bootstrap.js">
     <link rel="stylesheet" href="./css/dashstyle.css">
-    <title>Footer</title>
+    <title>Dashboard</title>
 </head>
-</head>
+<?php include('../dashboard/include/layout/header.php'); ?>
+<?php include('../dashboard/include/layout/footer.php'); ?>
 <body>
-    <header>
-        Welcome Footer
-    </header>
+    
 </body>
 </html>
